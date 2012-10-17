c979ef9986154af002bb8aceaf2c1879705fb214
Jay Jones <jay@jay-HP-Compaq-dc7600-Convertible-Minitower.(none)>
2012-10-16 17:20:56 -0700

    Added Micropost model

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -31,11 +31,11 @@ module SessionsHelper
   	end
 
   	def redirect_back_or(default)
-  		redirect_to(session[:return_to] || default)
-  		session.delete(:return_to)
-  	end
+    	redirect_to(session[:return_to] || default)
+    	session.delete(:return_to)
+ 	end
 
-  	def store_location
-  		session[:return_to] = request.url
-  	end
+  def store_location
+    session[:return_to] = request.url
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,7 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:microposts) }
 	it { should respond_to(:admin) }
 
 	it { should be_valid }
@@ -123,5 +124,31 @@ describe User do
 	describe "with a password that's too short" do
 		before { @user.password = @user.password_confirmation = "a" * 5}
 		it { should be_invalid}
+	end
+
+	describe "microposts associations" do
+
+		before { @user.save }
+
+		let!(:older_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+		end
+
+		let!(:newer_micropost) do
+			FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+		end
+
+		it "should have the right micropost in the right order" do
+			@user.microposts.should == [newer_micropost, older_micropost]
+		end
+
+		it "should destroy associated micropost" do
+			microposts = @user.microposts.dup
+			@user.destroy
+			microposts.should_not be_empty
+			microposts.each do |micropost|
+				Micropost.find_by_id(micropost.id).should be_nil
+			end
+		end
 	end
 end
