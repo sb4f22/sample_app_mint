716f28b6c01a4fb91aa7d6ba1c247b18aca27844
Jay Jones <jay@jay-HP-Compaq-dc7600-Convertible-Minitower.(none)>
2012-10-18 11:34:34 -0700

    Add user micropost

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -72,23 +72,35 @@ describe "Authentication" do
 					it { should have_selector('title', text: 'Sign in') }
 				end
 			end
+				
+			describe "in the Micropost controller" do
 
-		end
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { response.should redirect_to(signin_path) }
+				end
 
-		describe "as wrong user" do
-			let(:user) { FactoryGirl.create(:user) }
-			let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-			before { sign_in user }
-
-			describe "visiting USers#edit page" do
-				before { visit edit_user_path(wrong_user) }
-				it { should_not have_selector('title', text: full_title('Edit user')) }
+				describe "submitting to the destroy action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          			specify { response.should redirect_to(signin_path) }
+				end
 			end
 
-			describe "submitting a PUT request to the Users#update action" do
-				before { put user_path(wrong_user) }
-				specify { response.should redirect_to(root_path)}
-			end 
+			describe "as wrong user" do
+				let(:user) { FactoryGirl.create(:user) }
+				let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+				before { sign_in user }
+
+				describe "visiting Users#edit page" do
+					before { visit edit_user_path(wrong_user) }
+					it { should_not have_selector('title', text: full_title('Edit user')) }
+				end
+
+				describe "submitting a PUT request to the Users#update action" do
+					before { put user_path(wrong_user) }
+					specify { response.should redirect_to(root_path)}
+				end 
+			end
 		end	
 
 		describe "as non-admin user" do
